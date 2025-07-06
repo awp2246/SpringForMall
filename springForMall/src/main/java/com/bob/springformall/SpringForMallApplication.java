4c346ce908b5a5b922b15f70a2fdbc15d0714dcc
bob <awp2246@gmail.com>
2025-07-06 17:42:10 +0800

    spring 1

---
+++ springForMall/src/main/java/com/bob/springformall/SpringForMallApplication.java
@@ -0,0 +1,13 @@
+package com.bob.springformall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringForMallApplication.class, args);
+    }
+
+}
