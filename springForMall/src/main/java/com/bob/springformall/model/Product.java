cb9252b4f26c920d607e1746dc1c2abe641fc055
bob <awp2246@gmail.com>
2025-07-12 16:55:54 +0800

    商品功能-商品category使用Enum定義

--- springForMall/src/main/java/com/bob/springformall/model/Product.java
+++ springForMall/src/main/java/com/bob/springformall/model/Product.java
@@ -1,11 +1,13 @@
 package com.bob.springformall.model;
 
+import com.bob.springformall.constant.ProductCategory;
+
 import java.util.Date;
 
 public class Product {
     private int productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
@@ -31,11 +33,11 @@ public class Product {
         this.productName = productName;
     }
 
-    public String getCategory() {
+    public ProductCategory getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(ProductCategory category) {
         this.category = category;
     }
 
--- springForMall/src/main/java/com/bob/springformall/rowmapper/ProductRowMapper.java
+++ springForMall/src/main/java/com/bob/springformall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.bob.springformall.rowmapper;
 
+import com.bob.springformall.constant.ProductCategory;
 import com.bob.springformall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,12 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(rs.getInt("product_id"));
         product.setProductName(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+
+
+        String categoryStr = rs.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
         product.setImageUrl(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));
